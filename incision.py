018603f670e49009e000fb6e583deed3164c14c4
janburian <jburian@students.zcu.cz>
2023-05-17 11:57:43 +0200

    Added potential method for counting angles
    Added method which can be used for counting angles between incision and stitches

--- incision.py
+++ incision.py
@@ -1,16 +1,20 @@
+# Imports
 import numpy as np
 import skimage.io
 import skimage.feature
 import matplotlib.pyplot as plt
+import os
+import cv2
+import math
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+# Imports from own modules
 from thresholdOtsu import otsu
 from skelet import skeletonize
 from dilatation import dilatation
 from edgesSegmenation import edges_detection
 from incisionPolyline import polyline_detection
-import os
-import cv2
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 
 
 def detect_incision(image, false_detected_incision):
@@ -179,6 +183,7 @@ def average_coordinates(start_points, end_points):
 
         # returning the proper format
         keypoints_out.append(np.array([[start_x, start_y, end_x, end_y]]))
+
     return keypoints_out
 
 
@@ -241,6 +246,38 @@ def keypoints_postprocessing(keypoints, img, keypoints_type):
             return keypoints  # returning the (x1,y1) (x2,y2)
     elif keypoints_type == "stitch":
         a = 0
+
+
+def angle_between_lines(p1_start, p1_end, p2_start, p2_end):
+    '''
+    Could be used for counting angles between incision and stitches
+    :param p1_start:
+    :param p1_end:
+    :param p2_start:
+    :param p2_end:
+    :return:
+    '''
+    # Compute the slopes of the lines
+    dx1 = p1_end[0] - p1_start[0]
+    dy1 = p1_end[1] - p1_start[1]
+    dx2 = p2_end[0] - p2_start[0]
+    dy2 = p2_end[1] - p2_start[1]
+
+    # Calculate the angles using arctan2
+    theta1 = np.arctan2(dy1, dx1)
+    theta2 = np.arctan2(dy2, dx2)
+
+    # Compute the angle between the lines
+    angle = abs(theta2 - theta1)
+
+    # Normalize the angle to the range [0, pi/2]
+    if angle > math.pi / 2:
+        angle = math.pi - angle
+
+    # Convert angle to degrees
+    angle_degrees = math.degrees(angle)
+
+    return angle_degrees
 
 
 if __name__ == "__main__":
--- testovaci.py
+++ testovaci.py
@@ -3,13 +3,48 @@ from pathlib import Path
 import os
 import skimage.io
 import cv2
+import math
+import numpy as np
 
-path_images = Path("./images")
-images_list = os.listdir(path_images)
 
-for image in images_list:
-    # load the incision image
-    # image = test3
-    incision = skimage.io.imread(os.path.join(path_images, image))
-    plt.imshow(incision)
-    plt.show()
+def angle_between_lines(p1_start, p1_end, p2_start, p2_end):
+    # Compute the slopes of the lines
+    dx1 = p1_end[0] - p1_start[0]
+    dy1 = p1_end[1] - p1_start[1]
+    dx2 = p2_end[0] - p2_start[0]
+    dy2 = p2_end[1] - p2_start[1]
+
+    # Calculate the angles using arctan2
+    theta1 = np.arctan2(dy1, dx1)
+    theta2 = np.arctan2(dy2, dx2)
+
+    # Compute the angle between the lines
+    angle = abs(theta2 - theta1)
+
+    # Normalize the angle to the range [0, pi/2]
+    if angle > math.pi / 2:
+        angle = math.pi - angle
+
+    # Convert angle to degrees
+    angle_degrees = math.degrees(angle)
+
+    return angle_degrees
+
+
+x_1 = [30, 30]
+y_1 = [10, 50]
+
+x_2 = [25, 35]
+y_2 = [20, 35]
+
+plt.plot(x_1, y_1)
+plt.plot(x_2, y_2)
+plt.show()
+
+p1_start = [30, 10]
+p1_end = [30, 50]
+p2_start = [25, 20]
+p2_end = [35, 35]
+
+angle = angle_between_lines(p1_start, p1_end, p2_start, p2_end)
+print()
